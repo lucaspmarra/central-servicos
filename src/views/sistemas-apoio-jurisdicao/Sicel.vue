b1d6b50c6206ce943a7f7febc46e7f588e7c8d86
Lucas Paulino Marra <lucas.marra.terceirizado@tre-go.jus.br>
2022-09-16 16:02:47 -0300

    atualização na página do SICEL

--- src/views/sistemas-apoio-jurisdicao/Sicel.vue
+++ src/views/sistemas-apoio-jurisdicao/Sicel.vue
@@ -2,30 +2,35 @@
   <div class="container-content">
     <section class="pb-5">
       <div class="text-center pt-4">
-        <img src="@/assets/sicel.png" alt="img">
+        <img src="@/assets/sicel.png" alt="img" />
       </div>
       <b-link class="pb-5" to="/sistemas-apoio-jurisdicao">Voltar</b-link>
       <div class="pt-5">
         <h4>Para que serve:</h4>
-        <p>Sistema utilizado para o cadastramento de roteiros e preenchimento de relatórios de correições e inspeções,
-          bem como para a confecção do relatório anual de atividades da Vice-Presidência e Corregedoria Regional.</p>
+        <p>
+          Sistema utilizado para o cadastramento de roteiros e preenchimento de
+          relatórios de correições e inspeções.
+        </p>
       </div>
 
       <div class="pt-3">
         <h4>Link de acesso:</h4>
-        <p><a href="http://sicel.tse.jus.br/autentica">http://sicel.tse.jus.br/autentica</a>
+        <p>
+          <a href="https://sinco.tse.jus.br/">https://sinco.tse.jus.br/</a>
         </p>
       </div>
 
       <div class="pt-3">
         <h4>Forma de acesso:</h4>
-        <p>E-mail institucional e senha cadastrados no sistema.</p>
+        <p>O sistema é acessado pelo Odin 3.</p>
       </div>
 
       <div class="pt-3">
         <h4>Solicitação de acesso:</h4>
-        <p>Via SEI, preencher o "Formulário - Acesso a Sistema - Corregedoria" e enviar para a Assessoria de
-          Planejamento, Governança e Gestão da Vice-Presidência e Corregedoria</p>
+        <p>
+          Via SEI, preencher o "Formulário - Acesso a Sistema - Corregedoria" e
+          enviar para a Assistência de Projetos e Apoio Administrativo (APAAD).
+        </p>
       </div>
 
       <!--      <div class="pt-3">-->
@@ -46,17 +51,14 @@
       <!--      </div>-->
     </section>
 
-
     <b-link class="pb-5" to="/sistemas-apoio-jurisdicao">Voltar</b-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Sicel"
-}
+  name: "Sicel",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/sistemas-apoio-jurisdicao/SistemasApoioJurisdicao.vue
+++ src/views/sistemas-apoio-jurisdicao/SistemasApoioJurisdicao.vue
@@ -1,32 +1,43 @@
 <template>
   <div class="pt-5" id="content">
-    <div class="text-center pb-4 text-uppercase"><h2>Sistemas de Apoio à Jurisdição</h2></div>
+    <div class="text-center pb-4 text-uppercase">
+      <h2>Sistemas de Apoio à Jurisdição</h2>
+    </div>
     <b-card-group deck class="text-center">
       <b-card>
-        <img src="@/assets/png/arrest.png" alt="Banco Nacional de Monitoramento de Prisões" class="card-img">
+        <img
+          src="@/assets/png/arrest.png"
+          alt="Banco Nacional de Monitoramento de Prisões"
+          class="card-img"
+        />
         <h4>BNMP</h4>
         <b-card-text>
           <p>Banco Nacional de Monitoramento de Prisões</p>
-
         </b-card-text>
         <b-link class="stretched-link" to="/navegacao/bnmp"></b-link>
       </b-card>
 
       <b-card>
-        <img src="@/assets/png/protection.png" alt="Sistema Nacional de Informações Criminais da Polícia Federal"
-             class="card-img">
+        <img
+          src="@/assets/png/protection.png"
+          alt="Sistema Nacional de Informações Criminais da Polícia Federal"
+          class="card-img"
+        />
         <h4>SINIC/PF</h4>
         <b-card-text>
           <p>
             Sistema Nacional de Informações Criminais da Polícia Federal
           </p>
-
         </b-card-text>
         <b-link class="stretched-link" to="/navegacao/sinic"></b-link>
       </b-card>
 
       <b-card>
-        <img src="@/assets/png/document.png" alt="Sistema Eletrônico de Execução Unificado" class="card-img">
+        <img
+          src="@/assets/png/document.png"
+          alt="Sistema Eletrônico de Execução Unificado"
+          class="card-img"
+        />
         <h4>SEEU</h4>
         <b-card-text>
           <p>
@@ -39,29 +50,39 @@
 
     <b-card-group deck class="text-center pt-4">
       <b-card>
-        <img src="@/assets/png/law.png" alt="Sistema de Gestão de Tabelas Processuais Unificadas" class="card-img">
+        <img
+          src="@/assets/png/law.png"
+          alt="Sistema de Gestão de Tabelas Processuais Unificadas"
+          class="card-img"
+        />
         <h4>SGT</h4>
         <b-card-text>
           <p>
             Sistema de Gestão de Tabelas Processuais Unificadas
           </p>
-
         </b-card-text>
         <b-link class="stretched-link" to="/navegacao/sgt"></b-link>
       </b-card>
 
       <b-card>
-        <img src="@/assets/png/police-station.png" alt="Sistema de Inspeções e Correições Eleitorais"
-             class="card-img">
+        <img
+          src="@/assets/png/police-station.png"
+          alt="Sistema de Inspeções e Correições Eleitorais"
+          class="card-img"
+        />
         <h4>SICEL</h4>
         <b-card-text>
-          Sistema de Inspeções e Correições Eleitorais
+          Sistema de Inspeções e Correições da Justiça Eleitoral - SInCO
         </b-card-text>
         <b-link class="stretched-link" to="/navegacao/sicel"></b-link>
       </b-card>
 
       <b-card>
-        <img src="@/assets/png/riot-police.png" alt="Banco de Peritos" class="card-img">
+        <img
+          src="@/assets/png/riot-police.png"
+          alt="Banco de Peritos"
+          class="card-img"
+        />
         <h4>Banco de Peritos</h4>
         <b-card-text>
           Peritos cadastrados pela Corregedoria-Geral do TJGO
@@ -71,14 +92,14 @@
     </b-card-group>
 
     <b-link class="pb-5" to="/">Voltar</b-link>
-<!--    <a href="/central-de-sistemas">Voltar</a>-->
+    <!--    <a href="/central-de-sistemas">Voltar</a>-->
   </div>
 </template>
 
 <script>
 export default {
-  name: "SistemasApoioJurisdicao"
-}
+  name: "SistemasApoioJurisdicao",
+};
 </script>
 
 <style scoped>
@@ -92,7 +113,7 @@ export default {
 .card:hover {
   transform: translateY(-5px) scale(1.005) translateZ(0);
   box-shadow: 0 24px 36px rgba(0, 0, 0, 0.11),
-  0 24px 46px var(--box-shadow-color);
+    0 24px 46px var(--box-shadow-color);
 }
 
 .card-img {
